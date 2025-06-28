95352e0fa8bcd5d923b77cda67cb390ac4d1bccc
scrubysponge <imaankhalid2006@gmail.com>
2025-06-28 19:52:53 +0800

    add blink-cmp-words plugin
    blink-cmp-words adds thesaurus/dictionary snippets

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -1,29 +1,18 @@
 return {
-	'saghen/blink.cmp',
-	dependencies = { 'L3MON4D3/LuaSnip' },
+	"saghen/blink.cmp",
+	dependencies = { "L3MON4D3/LuaSnip", "archie-judd/blink-cmp-words" },
 
-	version = '1.*',
+	version = "1.*",
 
 	---@module 'blink.cmp'
 	---@type blink.cmp.Config
 	opts = {
-		-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-		-- 'super-tab' for mappings similar to vscode (tab to accept)
-		-- 'enter' for enter to accept
-		-- 'none' for no mappings
-		--
-		-- All presets have the following mappings:
-		-- C-space: Open menu or open docs if already open
-		-- C-n/C-p or Up/Down: Select next/previous item
-		-- C-e: Hide menu
-		-- C-k: Toggle signature help (if signature.enabled = true)
-		--
 		-- See :h blink-cmp-config-keymap for defining your own keymap
-		keymap = { preset = 'default' },
+		keymap = { preset = "default" },
 
 		appearance = {
-		nerd_font_variant = 'iosevka'
-	},
+			nerd_font_variant = "iosevka",
+		},
 
 		-- (Default) Only show the documentation popup when manually triggered
 		completion = { documentation = { auto_show = true } },
@@ -31,7 +20,43 @@ return {
 		-- Default list of enabled providers defined so that you can extend it
 		-- elsewhere in your config, without redefining it, due to `opts_extend`
 		sources = {
-			default = { 'lsp', 'path', 'snippets', 'buffer' },
+			default = { "lsp", "path", "snippets", "buffer" },
+
+			per_filetype = {
+				text = { "dictionary", "thesaurus" },
+				markdown = { "dictionary", "thesaurus" },
+			},
+
+			providers = {
+				thesaurus = {
+					name = "blink-cmp-words",
+					module = "blink-cmp-words.thesaurus",
+					-- All available options
+					opts = {
+						-- A score offset applied to returned items.
+						-- By default the highest score is 0 (item 1 has a score of -1, item 2 of -2 etc..).
+						score_offset = 0,
+
+						-- Default pointers define the lexical relations listed under each definition,
+						-- see Pointer Symbols below.
+						-- Default is as below ("antonyms", "similar to" and "also see").
+						pointer_symbols = { "!", "&", "^" },
+					},
+				},
+
+				dictionary = {
+					name = "blink-cmp-words",
+					module = "blink-cmp-words.dictionary",
+					-- All available options
+					opts = {
+						-- The number of characters required to trigger completion.
+						-- Set this higher if completion is slow, 3 is default.
+						dictionary_search_threshold = 3,
+						score_offset = 0,
+						pointer_symbols = { "!", "&", "^" },
+					},
+				},
+			},
 		},
 
 		-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
@@ -39,8 +64,8 @@ return {
 		-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
 		--
 		-- See the fuzzy documentation for more information
-		fuzzy = { implementation = "prefer_rust_with_warning" }
+		fuzzy = { implementation = "prefer_rust_with_warning" },
 	},
 
-	opts_extend = { "sources.default" }
+	opts_extend = { "sources.default" },
 }
