1d3ff78272bfddc9bd3a9b0807cf3d51530074a7
scrubysponge <imaankhalid2006@gmail.com>
2025-07-07 21:21:40 +0800

    lil treesitter change

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,17 +1,17 @@
 -- NOTE: neovim treesitter is experimental
 return {
 	"nvim-treesitter/nvim-treesitter",
-	branch = "main", -- NOTE: check for updates in main branch after every update
+	branch = "main",
 	lazy = false,
 	build = ":TSUpdate",
 	event = "BufReadPre",
 
 	-- nvim-treesitter requires config instead of opts
 	config = function()
-		require("nvim-treesitter").setup {
+		require("nvim-treesitter").setup({
 			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
 			highlight = { enable = true },
-			incremental_selection = { enable = true }
-		}
-	end
+			incremental_selection = { enable = true },
+		})
+	end,
 }
